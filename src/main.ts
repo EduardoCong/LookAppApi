6f09c7bdf91d3844c94bf362fb8f19c26d8c678c
CarlosMejiaUTM <134000850+CarlosMejiaUTM@users.noreply.github.com>
2025-10-30 17:49:12 -0600

    Enable CORS for all origins in main.ts (Ctm ricardo)
    Added CORS configuration to allow requests from any origin.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+// FileName: main.ts
+// Path: main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,9 +8,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // --- CONFIGURACIÓN DE CORS PARA ACEPTAR TODO ---
+  // Esto permite peticiones desde CUALQUIER origen (*).
+  // Es ideal para pruebas, pero menos seguro para producción.
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  // ----------------------------------------------
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
